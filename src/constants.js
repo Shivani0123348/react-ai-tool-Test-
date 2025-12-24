563ded497f8ca53c434507787240728058738fb6
Shivani0123348 <shivanibajpai542@gmail.com>
2025-12-24 16:00:14 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA1dtHPc6cx1dnTI4R--j9SbPjFAhW9IXs"
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBkRg_ZqY0jyc4S5qh3WyyzjGspYPZTwfg"
