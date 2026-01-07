4425327b51d2719064c9ad91c8999ac082d6c777
Shivani0123348 <shivanibajpai542@gmail.com>
2026-01-07 13:20:10 +0530

    Update API key in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBkRg_ZqY0jyc4S5qh3WyyzjGspYPZTwfg"
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBVo7llfy4mqsKyOv7eslYCl9wvKhl5ODI"
